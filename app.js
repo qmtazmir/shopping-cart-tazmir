d29e078b01a8a8f47014c871fab559d41a76961d
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-02-18 23:41:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function updateCaseNumber(isIncreasing) {
+  const caseInput = document.getElementById("case-number");
+  let caseNumber = caseInput.value;
+  if (isIncreasing == true) {
+    caseNumber = parseInt(caseNumber) + 1;
+
+    // decreased number
+  } else if (caseNumber > 0) {
+    caseNumber = parseInt(caseNumber) - 1;
+  }
+
+  // update case total
+  const caseTotal = document.getElementById("case-total");
+  caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateCaseNumber(true);
+});
+
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateCaseNumber(false);
+});
