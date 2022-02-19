963b1d7273e8c77c87ea9af60cfa66958febb231
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-02-19 10:37:54 +0600

    updateProductNumber

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-function updateCaseNumber(isIncreasing) {
-  const caseInput = document.getElementById("case-number");
-  let caseNumber = caseInput.value;
-  if (isIncreasing == true) {
-    caseNumber = parseInt(caseNumber) + 1;
+function updateProductNumber(product, price, isIncreasing) {
+  const productInput = document.getElementById(product + "-number");
+  let productNumber = parseInt(productInput.value);
 
-    // decreased number
-  } else if (caseNumber > 0) {
-    caseNumber = parseInt(caseNumber) - 1;
+  if (isIncreasing == true) {
+    productNumber = productNumber + 1;
   }
 
-  // update case total
-  const caseTotal = document.getElementById("case-total");
-  caseTotal.innerText = caseNumber * 59;
+  if (isIncreasing == false && productNumber > 0) {
+    productNumber = productNumber - 1;
+  }
+
+  productInput.value = productNumber;
+
+  // --------------------okkkkkk----------------
+
+  //update total
+  const productTotal = document.getElementById(product + "-total");
+  productTotal.innerText = productNumber * price;
 }
 
+///phone increase decrease event
+
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updateProductNumber("phone", 1219, true);
+});
+
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateProductNumber("phone", 1219, false);
+});
+
+// handel case increase and dicrease
+
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  updateProductNumber("case", 59, true);
 });
 
 document.getElementById("case-minus").addEventListener("click", function () {
-  updateCaseNumber(false);
+  updateProductNumber("case", 59, false);
 });
